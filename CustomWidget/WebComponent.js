880702f0676e0247ebb6e697826c938930f0b4bd
sanvi0104 <126646290+sanvi0104@users.noreply.github.com>
2023-09-01 11:05:26 +0530

    Update WebComponent.js

--- CustomWidget/WebComponent.js
+++ CustomWidget/WebComponent.js
@@ -1,24 +1,31 @@
 (function () {
-  var tmpl = document.createElement("template");
-  tmpl.innerHTML = `<button type="button" id="myBtn">Helper Button</button>`;
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = 
+    `<button type="button" id="myBtn">Helper Button</button>` ;   
+   
+    class PerformanceHelp extends HTMLElement {
+        constructor() {
+            super();
+            this.init();           
+        }
 
-  class PerformanceHelp extends HTMLElement() {
-    constructor() {
-      super();
-      this.init();
+        init() {            
+              
+            let shadowRoot = this.attachShadow({mode: "open"});
+            shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this.addEventListener("click", event => {
+            var event = new Event("onClick");
+            this.fireChanged();           
+            this.dispatchEvent(event);
+            });           
+        }
+
+        fireChanged() {
+            console.log("OnClick Triggered");     
+            
+        }        
+        
     }
 
-    init() {
-      let shadowRoot = this.attachShadow({ mode: "open" });
-      shadowRoot.appendChild(tmpl.content.cloneNode(true));
-      this.addEventListener("click", (event) => {
-        var event = new Event("onClick");
-        this.fireChanged();
-        this.dispatchEvent(event);
-      });
-    }
-  }
-
-  customElements.define("custom-button", PerformanceHelp);
+    customElements.define('custom-button', PerformanceHelp);
 })();
-
